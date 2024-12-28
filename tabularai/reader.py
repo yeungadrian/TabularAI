e7c5cfd5bc41f39f4c5fb20079f54cee66ac71da
yeungadrian <yeungadrianjy@gmail.com>
2024-12-28 18:35:27 +0000

    clean reader

--- tabularai/reader.py
+++ tabularai/reader.py
@@ -11,31 +11,30 @@ from tabulate import tabulate
 from tabularai.settings import Settings
 
 
-class Sheet(BaseModel):
-    """Aasd."""
+class Table(BaseModel):
+    """Table model."""
 
     sheet_name: str
     data: list[list[int | float | str | bool | time | date | datetime | timedelta]]
     markdown: str
 
 
-def convert_excel(file: str | IO[bytes], settings: Settings | None = None) -> list[Sheet]:
+def convert_excel(file: str | IO[bytes], settings: Settings | None = None) -> list[Table]:
     """Convert an excel workbook into markdown."""
     if settings is None:
         settings = Settings()
-
     workbook = CalamineWorkbook.from_object(file)  # type: ignore
-
-    sheets: list[Sheet] = []
+    tables: list[Table] = []
     for sheet_name in workbook.sheet_names:
         calamine_sheet = workbook.get_sheet_by_name(sheet_name)
         tabular_data = calamine_sheet.to_python(skip_empty_area=settings.skip_empty_area)
+        cleaned_tabular_data = [[re.sub(r"\s+", " ", str(j)).strip() for j in i] for i in tabular_data]
         markdown = f"## Sheet: {sheet_name}\n\n"
         markdown += tabulate(
-            [[re.sub(r"\s+", " ", str(j)).strip() for j in i] for i in tabular_data],
+            cleaned_tabular_data,
             headers="firstrow",
             tablefmt=settings.tablefmt,
             showindex=settings.showindex,
         )
-        sheets.append(Sheet(sheet_name=sheet_name, data=tabular_data, markdown=markdown))
-    return sheets
+        tables.append(Table(sheet_name=sheet_name, data=tabular_data, markdown=markdown))
+    return tables
